33a46b89a73b6a91eda195c62c69ff53fded53e7
Josef Hobel <josef.hobel.ext@gmail.com>
2025-12-07 14:13:37 +0100

    c sharp project rework

--- CalculatorApp_C#/tests/CalculatorApp.Tests/CalculatorTests.cs
+++ CalculatorApp_C#/tests/CalculatorApp.Tests/CalculatorTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 
 namespace CalculatorApp.Tests
 {
@@ -16,28 +17,28 @@ namespace CalculatorApp.Tests
         public void Add_TwoPositiveNumbers_ReturnsCorrectSum()
         {
             var result = calculator.Add(2, 3);
-            Assert.AreEqual(5, result);
+            Assert.That(result, Is.EqualTo(5));
         }
 
         [Test]
         public void Subtract_TwoPositiveNumbers_ReturnsCorrectDifference()
         {
             var result = calculator.Subtract(5, 3);
-            Assert.AreEqual(2, result);
+            Assert.That(result, Is.EqualTo(2));
         }
 
         [Test]
         public void Multiply_TwoPositiveNumbers_ReturnsCorrectProduct()
         {
             var result = calculator.Multiply(2, 3);
-            Assert.AreEqual(6, result);
+            Assert.That(result, Is.EqualTo(6));
         }
 
         [Test]
         public void Divide_TwoPositiveNumbers_ReturnsCorrectQuotient()
         {
             var result = calculator.Divide(6, 3);
-            Assert.AreEqual(2, result);
+            Assert.That(result, Is.EqualTo(2));
         }
 
         [Test]
